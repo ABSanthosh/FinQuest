44c6aa8733c49c3000acf8ae7ccee3d2a6b79921
ABSanthosh <a.b.santhosh02@gmail.com>
2023-01-24 00:54:35 +0530

    feat: added fetching feature

--- pages/api/fetch-data.js
+++ pages/api/fetch-data.js
@@ -1,4 +1,32 @@
-export default (req, res)=>{
-    
-    res.status(200).json({name: "John Doe"})
-}
+export default async (req, res) => {
+  const spreadsheetId = "1SvyMyOn0r3SuKh9HLY8qodZ9w12qamf3JB9CI5UAqpQ";
+  const response = await fetch(
+    `https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json`
+  );
+  const result = await response.text();
+  result.replace(
+    /.*google.visualization.Query.setResponse\({(.*?)}\);?/s,
+    "{$1}"
+  );
+  console.log(result);
+  // const json = result;
+  const json = JSON.parse(
+    result.replace(
+      /.*google.visualization.Query.setResponse\({(.*?)}\);?/s,
+      "{$1}"
+    )
+  );
+
+  const headings = json.table.cols.map((item) => item.label);
+
+  let data = json.table.rows.map((item) => {
+    // console.log(item);
+    let row = {};
+    item.c.forEach((cell, idx) => {
+      row[headings[idx]] = cell?.v ?? null;
+    });
+    return row;
+  });
+
+  res.status(200).json({ data });
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
 import "../styles/routes/Home.scss";
 
 export default function Home() {
-  return <main className="HomeContainer">hi</main>;
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    const response = await fetch("/api/fetch-data");
+    const json = await response.json();
+    setData(json);
+  };
+
+  
+  // Uncomment this to fetch data every second(Make shift live update)
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     fetchData();
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return (
+    <main className="HomeContainer">
+      <button
+        onClick={async () => {
+          fetchData();
+        }}
+      >
+        Fetch
+      </button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </main>
+  );
 }
